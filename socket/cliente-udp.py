066ce9f71c8df221cdc98634095634b6cd7d77c5
Mateus Padilha <103007201+padirex@users.noreply.github.com>
2023-09-13 17:08:18 -0300

    Add files via upload

---
+++ socket/cliente-udp.py
@@ -0,0 +1,14 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+
+server_name = 'localhost'
+server_port = 12001
+
+client_socket = socket(AF_INET, SOCK_DGRAM)
+
+msg = input('Entre minúsculas \n')
+
+client_socket.sendto(msg.encode(), (server_name, server_port))
+msg_modificada, server_adddress = client_socket.recvfrom(2048)
+
+print(msg_modificada.decode())
+client_socket.close()
